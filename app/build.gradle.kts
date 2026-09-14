6dcdeb7645604b836696c0c16f07ed6808b62245
grafiti666666 <grafiti666666@gmail.com>
2026-09-14 23:20:57 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,15 @@ android {
     namespace = "com.example.minimalllauncher"
     compileSdk = 35
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
     defaultConfig {
         applicationId = "com.example.minimalllauncher"
         minSdk = 23
