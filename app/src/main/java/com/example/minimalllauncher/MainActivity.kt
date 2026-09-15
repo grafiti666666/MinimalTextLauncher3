8cface4b274cd5f052764b07f26d320f9ce05316
grafiti666666 <grafiti666666@gmail.com>
2026-09-15 02:06:35 +0200

    Update MainActivity.kt

--- app/src/main/java/com/example/minimalllauncher/MainActivity.kt
+++ app/src/main/java/com/example/minimalllauncher/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
